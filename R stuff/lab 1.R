524ed91319ab403f3b8f3f8d1900c826bf5b00ae
smgibson19 <smjgibson17@gmail.com>
2022-01-30 13:53:16 -0500

    testing push
    made changes and testing pushing to make sure it works :)

--- R stuff/lab 1.R
+++ R stuff/lab 1.R
@@ -3,3 +3,5 @@
 grumpData <- read.csv(file.choose(), header = T) # GPW3_GRUMP_SummaryInformation_2010.csv
 epi2010Data <- read.csv(file.choose(), header = T)# EPI2010
 
+View(epi2010Data)
+summary(epi2010Data$)

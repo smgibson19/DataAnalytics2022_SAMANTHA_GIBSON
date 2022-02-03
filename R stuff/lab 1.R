1cb13ffde573237ab67da5722d68ebe2e0e18eb9
smgibson19 <smjgibson17@gmail.com>
2022-02-02 22:45:16 -0500

    fixing bugs
    went back to classnotes to correct mistakes in code, got first part functional

--- R stuff/lab 1.R
+++ R stuff/lab 1.R
@@ -3,7 +3,7 @@
 grumpData <- read.csv(file.choose(), header = T) # GPW3_GRUMP_SummaryInformation_2010.csv
 epi2010Data <- read.csv(file.choose(), header = T)# EPI2010
 
-# EPI exploration
+
 # fixing first row to be header
 names(epi2010Data) <- as.matrix(epi2010Data[1, ])
 epi2010Data <- epi2010Data[-1, ]
@@ -12,21 +12,23 @@ epi2010Data[] <- lapply(epi2010Data, function(x)
 View(epi2010Data)
 
 
-# exploring the distribution
-fivenum(epi2010Data$ENVHEALTH, na.rm=TRUE)
+# ENVHEALTH
+fivenum(epi2010Data$ENVHEALTH)
 stem(epi2010Data$ENVHEALTH)		 # stem and leaf plot
-hist(epi2010Data$ENVHEALTH, prob=TRUE, main = "Histogram of ENVHEALTH", xlab = "ENVHEALTH")
-lines(density(epi2010Data$ENVHEALTH, na.rm=TRUE, bw=2.), col = "red")
+
+hist(epi2010Data$ENVHEALTH)
+hist(epi2010Data$ENVHEALTH, seq(0.05, 100, 1), prob = T, main = "Histogram of ENVHEALTH", xlab = "ENVHEALTH",)
+lines(density(epi2010Data$ENVHEALTH, na.rm = T, bw = 1),  col = "red")
 rug(epi2010Data$ENVHEALTH) 
 
 plot(ecdf(epi2010Data$ENVHEALTH), do.points=FALSE, verticals=TRUE, main = "Plot of ENVHEALTH") 
 par(pty="s") 
+
 qqnorm(epi2010Data$ENVHEALTH); qqline(epi2010Data$ENVHEALTH)
 
-x <- rnorm(epi2010Data$ENVHEALTH)
-y <- rnorm(epi2010Data$ECOSYSTEM)
-qqplot(x, y, xlab = "ENVHEALTH", ylab = "ECOSYSTEM", main = "Q-Q Plot of ENVHEALTH")
-qqline(x)
+a <- seq(0.05, 100, 1)
+qqplot(qt(ppoints(250), df = 5), a, xlab = "ENVHEALTH", ylab = "ECOSYSTEM", main = "Q-Q Plot of ENVHEALTH")
+qqline(a)
 
 boxplot(epi2010Data$ENVHEALTH,epi2010Data$ECOSYSTEM)
 
@@ -38,8 +40,5 @@ hist(epiENV, prob=TRUE, main = "Histogram of ENVHEALTH", xlab = "ENVHEALTH")
 
 # Grump exploration
 View(grumpData) # don't need to fix the header
-
-
-
 
 

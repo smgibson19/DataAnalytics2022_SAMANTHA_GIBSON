e6ad517c7aa548a82ac5949d61b600fa8c19fc27
smgibson19 <smjgibson17@gmail.com>
2022-03-31 10:29:29 -0400

    lab 6 titanic ctree
    ctree code for lab 6, I think lab 6 should be done now :)

--- lab 6.R
+++ lab 6.R
@@ -23,24 +23,15 @@ rsq.rpart(tittanicPart) # visualize cross-validation results
 
 
 # ctree
-
-
-# makes a decision tree looking plot, plot makes the lines, text adds text to it
-swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
-plot(swiss_rpart) # try some different plot options
-text(swiss_rpart) # try some different text options
-
 # makes a fancier graph with little graphs going off of the decision tree
-treeSwiss<-ctree(Species ~ ., data=iris)
-plot(treeSwiss)
+treeTitanic<-ctree(Survived ~ ., data = Titanic)
+plot(treeTitanic)
 
 # prints a table of the number of trees, responses, inputs, and observations
-cforest(Species ~ ., data=iris, controls=cforest_control(mtry=2, mincriterion=0))
-treeFert<-ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
-cforest(Fertility ~ Agriculture + Education + Catholic, data = swiss, controls=cforest_control(mtry=2, mincriterion=0))
+cforest(Survived ~ ., data = Titanic, controls=cforest_control(mtry=2, mincriterion=0))
 
 # creates a graph based off of all of the information in the data frame
-tr <- tree(Species ~ ., data=iris)
+tr <- tree(Survived ~ ., data=Titanic)
 tr
 tr$frame
 plot(tr)
@@ -59,8 +50,3 @@ importance(fitT) # importance of each predictor
 varImpPlot(fitT)
 plot(fitT)
 getTree(fitT,1, labelVar=TRUE)
-
-
-
-
-# rpart, ctree, hclust,

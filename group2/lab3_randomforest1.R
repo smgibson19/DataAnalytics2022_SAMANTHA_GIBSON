940ae075f71e7d3ac04b3ec919c3d95b26c9f80f
smgibson19 <smjgibson17@gmail.com>
2022-04-24 20:00:42 -0400

    random forest
    getting this snippet to work :)

--- group2/lab3_randomforest1.R
+++ group2/lab3_randomforest1.R
@@ -1,5 +1,6 @@
 # import data
 library(randomForest)
+library(rpart)
 fitKF <- randomForest(Kyphosis ~ Age + Number + Start, data=kyphosis)
 print(fitKF) 	# view results
 importance(fitKF) # importance of each predictor
